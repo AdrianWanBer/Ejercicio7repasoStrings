491d87cc29f2c2afe8b902faaf32f10a20b1e059
DESKTOP-3T85H9P\Adrian <csi23-awanuer@colegioaltair.net>
2024-03-12 14:20:07 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio7repasoStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ejercicio6repasoStrings
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Escribe su cadena de texto");
+            string cadena = Console.ReadLine();
+            string[] cadenaTexto = cadena.Split(" ");
+            Console.WriteLine($"La cadena tiene: {cadenaTexto.Length} palabras");
+            
+            
+            /*for(int i = 0; i < cadenaTexto.Length; i++)
+            {
+                Console.WriteLine(cadenaTexto[i]);
+            }*/
+        }
+    }
+}
